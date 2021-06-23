99713ba54b15eb777e101ca6872175ad1d5b0ba7
saikumar0204 <saichodipilli@gmail.com>
2021-06-23 17:54:53 +0530

    final commit

--- index.js
+++ index.js
@@ -14,16 +14,39 @@ mongoose.connection.on('open',()=>{
 })
 
 const Task= require('./models/task')
+//Dropping collections to empty previous data
+mongoose.connection.collections.tasks.drop().then(()=>{})
+
 
 //Create
-const newTask= new Task({
+//Task1
+var newTask= new Task({
   Description:"NOde",
+  Completed:true
+})
+newTask.save()
+
+
+//Task2
+newTask= new Task({
+  Description:"Mongo",
   Completed:false
 })
-newTask.save().then(res=>{
-  // console.log(res);
-  console.log('SAved Successfully')
+newTask.save()
+
+//Task3
+ newTask= new Task({
+  Description:"Js",
+  Completed:true
 })
+newTask.save()
+
+//Task4
+newTask= new Task({
+  Description:"Express",
+  Completed:false
+})
+newTask.save()
 
 //Read
 Task.find({Completed:false}).then(res=>{
@@ -39,8 +62,9 @@ Task.updateMany({Completed:false},{Completed:true}).then((res)=>{
   console.log(res,'Updated Succesfuly')
 })
 
+
 var id;
 //Delete
-Task.findOneAndDelete({_id:newTask.id}).then(res=>{
+Task.findOneAndDelete({_id:id}).then(res=>{
   console.log('Record deleted Successfully')
 })
